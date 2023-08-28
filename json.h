6b0eb1c19336b01c9b53e746a1869d05ab485d07
iamaditya <lionadi1403@gmail.com>
2023-08-29 02:09:06 +0530

    Updated print function to print the json TODO : Needs more modification.
    Added json.cpp file

--- json.h
+++ json.h
@@ -1,24 +1,16 @@
+#ifndef jsonclass
+#define jsonclass
 #include <unordered_map>
+#include <string>
+#include<iostream>
 
 template<typename Key, typename Value>
 class aajson{
-
     private:
         std::unordered_map<Key, Value> _content;
-
-    
     public:
         aajson();
-
+        void printJson();
         Value& operator[](const Key& key);
 };
-
-template<typename Key, typename Value>
-aajson<Key, Value>::aajson(){
-
-}
-
-template<typename Key, typename Value>
-Value& aajson<Key, Value>::operator[](const Key& key){
-   return _content[key];
-}
+#endif
